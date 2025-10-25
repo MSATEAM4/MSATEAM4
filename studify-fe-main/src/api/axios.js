e04cb7a0475e4cbb98d15d48ebfe986c02503e3c
조성민 <csmin0203@gmail.com>
2025-10-25 12:33:24 +0900

    FE: Vercel 환경 변수를 사용 axios baseURL 수정

--- studify-fe-main/src/api/axios.js
+++ studify-fe-main/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",  // API Gateway를 통한 통합 진입점
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true,
 });
 
